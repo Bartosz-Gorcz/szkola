6cf0b7dfcac54baca1989f783f39f51e61bf6809
Bartosz-Gorcz <bartekbatr003@gmail.com>
2025-10-03 12:20:38 +0200

    Add files via upload

---
+++ zad3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int Fib(int n)
+{
+    if(n==1 || n==2){
+        return 1;
+    }else{
+        return Fib(n-1)+Fib(n-2);
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Podaj liczbe"<<endl;
+    cin>>n;
+    
+    Fib(n);
+    cout<<Fib(n);
+    return 0;
+    
+}
